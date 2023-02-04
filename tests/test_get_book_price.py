69b83c586e64c4b5e505a73f96f657d9b8cf9040
lzieniew <lukzieniewicz@gmail.com>
2023-02-04 14:12:55 +0100

    Tested not found book

--- tests/test_get_book_price.py
+++ tests/test_get_book_price.py
@@ -5,7 +5,7 @@ from src.amazon_books_scraper.enums import BookType
 def test_get_book_price_simple():
     book_name = 'AI and ML Powering the Agents of Automation'
     author = 'Deepika M, Vijay Cuddapah, Amitendra Srivastava, Srinivas Mahankali'
-    price = get_book_price(human_name=book_name, author=author)['price']
+    price = get_book_price(human_name=book_name, author=author, book_type=BookType.EBOOK)['price']
     assert price == '$9.95'
 
 
@@ -13,7 +13,7 @@ def test_get_book_price_unity_ai():
     book_name = 'Unity Artificial Intelligence Programming'
     author = 'Dr. Davide Aversa'
     publisher = 'packt'
-    price = get_book_price(human_name=book_name, author=author, publisher=publisher)
+    price = get_book_price(human_name=book_name, author=author, publisher=publisher, book_type=BookType.EBOOK)
     assert price['price'] == '$18.49'
 
 
@@ -21,11 +21,22 @@ def test_get_book_price_kindle_not_first_in_string():
     book_name = 'Bible 101'
     author = 'Edward D. Gravely'
     publisher = 'Adams Media'
-    price = get_book_price(human_name=book_name, author=author, publisher=publisher)
+    price = get_book_price(human_name=book_name, author=author, publisher=publisher, book_type=BookType.EBOOK)
     assert price['price'] == '$10.99'
+
 
 def test_get_book_price_audible_audiobook():
     book_name = 'The Shining'
     author = 'Stephen King'
     price = get_book_price(human_name=book_name, author=author, book_type=BookType.AUDIOBOOK)
     assert price['price'] == '$20.42'
+
+
+def test_get_not_found_book():
+    book_name = 'Pathfinder Adventure Path: Abomination Vaults'
+    publisher = 'Paizo Inc.'
+    price = get_book_price(human_name=book_name, publisher=publisher, book_type=BookType.EBOOK)
+    assert price['price'] == ''
+
+def test_get_not_found_book():
+    pass
