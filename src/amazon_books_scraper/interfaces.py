a27ccdb13d67d5dec62fdd37f4b446efce5f3796
lzieniew <lukzieniewicz@gmail.com>
2023-02-21 01:27:08 +0100

    Add referer header

--- src/amazon_books_scraper/interfaces.py
+++ src/amazon_books_scraper/interfaces.py
@@ -14,7 +14,8 @@ def get_amazon_product_info(search_name: str, book_type: BookType, human_name: s
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/105.0',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5'
+        'Accept-Language': 'en-US,en;q=0.5',
+        'referer': 'https://google.com',
 
     }
     response = requests.get(url=AMAZON_SEARCH_URL, params=params, headers=headers)
